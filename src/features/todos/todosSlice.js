2e7b4e04cc58391add470ccca92f27e009c11c32
lekan.akinremi@yahoo.com <reactmonk@olamilekan-MacBook-Pro-2.local>
2020-09-28 16:04:36 +0100

    first commit

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+let nextTodoId = 0;
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState: [],
+  reducers: {
+    addTodo: {
+      reducer: (state, action) => {
+        const { id, text } = action.payload;
+        state.push({ id, text, completed: false });
+      },
+      prepare: (text) => {
+        return { payload: { text, id: nextTodoId++ } };
+      },
+    },
+    toggleTodo: (state, action) => {
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+  },
+});
+
+export default todosSlice.reducer;
+
+export const { addTodo, toggleTodo } = todosSlice.actions;
+
+/**
+ * createSlice returns an object that looks like this:
+ * 
+  {
+  name: "todos",
+  reducer: (state, action) => newState,
+  actions: {
+    addTodo: (payload) => ({type: "todos/addTodo", payload}),
+    toggleTodo: (payload) => ({type: "todos/toggleTodo", payload})
+  },
+  caseReducers: {
+    addTodo: (state, action) => newState,
+    toggleTodo: (state, action) => newState,
+  }
+}
+ */
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import rootReducer from './reducers';
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = configureStore({ reducer: rootReducer });
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
